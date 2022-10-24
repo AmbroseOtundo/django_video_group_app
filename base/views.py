8c8832f18577f0501256a9f381043f8d856471c7
ambroseotundo <ambroseotundo9@gmail.com>
2022-10-24 10:51:58 -0400

    create the js to run the video and audio functionalities plus urls

--- base/views.py
+++ base/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from agora_token_builder import RtcTokenBuilder
 # Create your views here.
 def lobby(request):
     return render(request, 'base/lobby.html')
 
 def room(request):
     return render(request, 'base/room.html')
+
